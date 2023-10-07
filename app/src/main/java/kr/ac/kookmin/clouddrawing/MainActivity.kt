ddef2f463fc3a6d5fbe4327983bd66df41dcedc4
20203153 <123941587+20203153@users.noreply.github.com>
2023-10-07 17:09:10 +0900

    add MainActivity and upper bar

--- app/src/main/java/kr/ac/kookmin/clouddrawing/MainActivity.kt
+++ app/src/main/java/kr/ac/kookmin/clouddrawing/MainActivity.kt
@@ -1,97 +1,143 @@
 package kr.ac.kookmin.clouddrawing
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kr.ac.kookmin.clouddrawing.theme.ApplicationTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             make()
         }
     }
 
     companion object {
+        @OptIn(ExperimentalMaterial3Api::class)
         @Composable
         fun make() {
             return ApplicationTheme {
                 Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .shadow(
-                            elevation = 4.dp,
-                            spotColor = Color(0x40000000),
-                            ambientColor = Color(0x40000000)
-                        )
+                    Modifier
                         .width(390.dp)
                         .height(844.dp)
-                        .background(color = Color(0xFFE3ECFF))
+                        .background(color = Color(0xFFFFFFFF))
                 ) {
-                    Spacer(Modifier.height(294.dp))
-                    Image(
-                        painter = painterResource(R.drawable.loadinglogo),
-                        contentDescription = "image description",
-                        contentScale = ContentScale.None,
-                    )
-                    Spacer(Modifier.height(11.dp))
-                    Box(
-                        Modifier
-                            .width(239.dp)
-                            .height(27.dp)
-                    ) {
-                        Text(
-                            text = "나의 일상을 떠올리는 공간 ",
-                            style = TextStyle(
-                                fontSize = 22.sp,
-                                fontFamily = FontFamily(Font(R.font.inter)),
-                                fontWeight = FontWeight(600),
-                                color = Color(0xFF001753),
-                            )
-                        )
-                    }
-                    Spacer(Modifier.height(16.dp))
-                    Box(
-                        Modifier
-                            .width(157.dp)
-                            .height(37.dp)
-                    ) {
-                        Text(
-                            text = "나만 갖고 있는 추억의 장소를\n구름과 함께 떠올려보세요!",
-                            style = TextStyle(
-                                fontSize = 13.sp,
-                                fontFamily = FontFamily(Font(R.font.inter)),
-                                fontWeight = FontWeight(600),
-                                color = Color(0xFFA0A0A0),
-                                textAlign = TextAlign.Center,
-                            )
-                        )
-                    }
+
                 }
+                TopAppBar(
+                    navigationIcon = {
+                        Row {
+                            Spacer(Modifier.width(6.dp))
+                            CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
+                                IconButton(
+                                    onClick = {},
+                                    modifier = Modifier
+                                        .padding(0.dp)
+                                        .width(40.dp)
+                                        .height(38.dp)
+                                        .background(
+                                            Color(0xFFD2E1FF),
+                                            RoundedCornerShape(10.dp)
+                                        )
+                                ) {
+                                    Image(
+                                        modifier = Modifier
+                                            .height(17.1.dp)
+                                            .width(10.dp),
+                                        painter = painterResource(id = R.drawable.v_arrow_open),
+                                        contentDescription = "",
+                                        contentScale = ContentScale.Fit
+                                    )
+                                }
+                            }
+                            Spacer(Modifier.width(5.dp))
+                        }
+                    },
+                    title= {
+                        Row(
+                           modifier = Modifier
+                               .shadow(
+                                   elevation = 5.dp,
+                                   spotColor = Color(0x0D000000),
+                                   ambientColor = Color(0x0D000000)
+                               )
+                               .border(
+                                   width = 1.dp,
+                                   color = Color(0xFFF6F6F6),
+                                   shape = RoundedCornerShape(size = 10.dp)
+                               )
+                               .width(320.dp)
+                               .height(38.dp)
+                               .background(
+                                   color = Color(0xFFFFFFFF),
+                                   shape = RoundedCornerShape(size = 10.dp)
+                               )
+                        ) {
+                            BasicTextField(
+                                modifier = Modifier
+                                    .height(38.dp)
+                                    .width(285.dp),
+                                value = "",
+                                onValueChange = { },
+                                singleLine = true,
+                            )
+                            Spacer(Modifier.width(5.dp))
+                            CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
+                                IconButton(
+                                    onClick = {},
+                                    modifier = Modifier
+                                        .padding(0.dp)
+                                        .width(40.dp)
+                                        .height(38.dp)
+                                        .background(Color.Transparent)
+                                ) {
+                                    Image(
+                                        modifier = Modifier
+                                            .height(18.dp)
+                                            .width(18.dp),
+                                        painter = painterResource(id = R.drawable.v_home_search),
+                                        contentDescription = "",
+                                        contentScale = ContentScale.Fit
+                                    )
+                                }
+                            }
+                            Spacer(Modifier.width(11.28.dp))
+                        }
+                    },
+                    modifier = Modifier
+                        .background(color = Color.Transparent)
+                        .padding(top = 65.dp, start = 0.dp)
+                        .fillMaxWidth()
+                )
             }
         }
     }
@@ -99,6 +145,6 @@ class MainActivity : AppCompatActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun MainPreview() {
     MainActivity.make()
 }
