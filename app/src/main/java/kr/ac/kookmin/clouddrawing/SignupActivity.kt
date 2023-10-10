3757b9690de666f078e9b0f12a6e1031d2015125
20203153 <123941587+20203153@users.noreply.github.com>
2023-10-10 17:34:42 +0900

    set Loading, Signup activity layout is adaptive.

--- app/src/main/java/kr/ac/kookmin/clouddrawing/SignupActivity.kt
+++ app/src/main/java/kr/ac/kookmin/clouddrawing/SignupActivity.kt
@@ -1,17 +1,19 @@
 package kr.ac.kookmin.clouddrawing
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,7 +22,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kr.ac.kookmin.clouddrawing.theme.ApplicationTheme
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,36 +33,44 @@ class SignupActivity : AppCompatActivity() {
 
     companion object {
         @Composable
-        fun make() {
-            return ApplicationTheme {
+        fun make(onClickGoogle: () -> Unit = {}) {
+            return Box(
+                Modifier.background(color=Color(0xFFE3ECFF))
+            ) {
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
                     modifier = Modifier
-                        .width(390.dp)
-                        .height(844.dp)
-                        .background(color = Color(0xFFE3ECFF))
+                        .fillMaxSize(1f)
                 ) {
-                    Spacer(Modifier.height(294.dp))
                     Image(
                         painter = painterResource(R.drawable.loadinglogo),
                         contentDescription = "image description",
                         contentScale = ContentScale.None,
                     )
-                    Spacer(Modifier.height(290.dp))
-                    Button(
-                        onClick = { /*TODO*/ },
+                }
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Bottom,
+                    modifier = Modifier
+                        .fillMaxSize(1f)) {
+                    IconButton(
+                        onClick = onClickGoogle,
                         modifier = Modifier
                             .width(300.dp)
                             .height(40.dp)
-                            .background(color = Color(0xFF6891FF),
-                        shape = RoundedCornerShape(size = 12.dp))
+                            .background(
+                                color = Color(0xFF6891FF),
+                                shape = RoundedCornerShape(size = 12.dp)
+                            )
                     ) {
                         Image(
                             painter = painterResource(R.drawable.register_google),
                             contentDescription = "image description",
-                            contentScale = ContentScale.None,
+                            contentScale = ContentScale.None
                         )
                     }
+                    Spacer(Modifier.height(85.dp))
                 }
             }
         }
