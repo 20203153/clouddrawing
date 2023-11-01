222e5d71a320a9aea32f726dfc0ae64b25951387
20203153 <123941587+20203153@users.noreply.github.com>
2023-11-01 13:24:46 +0900

    CloudMindModal to responsive design (add below)
    - CloudMindModal to responsive design
    - add animation into CloudMindModal

--- app/src/main/java/kr/ac/kookmin/clouddrawing/components/CloudMindModal.kt
+++ app/src/main/java/kr/ac/kookmin/clouddrawing/components/CloudMindModal.kt
@@ -1,25 +1,30 @@
 package kr.ac.kookmin.clouddrawing.components
 
 
+import android.annotation.SuppressLint
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -27,6 +32,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -39,11 +45,20 @@ import androidx.compose.ui.unit.sp
 import kr.ac.kookmin.clouddrawing.R
 
 
-@Preview
+@SuppressLint("UnrememberedMutableState")
+@Preview(widthDp = 390, heightDp = 844)
 @Composable
 fun PreviewBackground() {
+    val isDrawerOpen = mutableStateOf(false)
+
     CMBackground {
-        CMMain()
+        Button(
+            onClick = { isDrawerOpen.value = !isDrawerOpen.value }
+        ) {
+            Text("Open/Close")
+        }
+
+        CMMain(isDrawerOpen = isDrawerOpen)
     }
 }
 
@@ -61,108 +76,154 @@ fun CMBackground(content: @Composable () -> Unit) {
 @Composable
 fun CMMain(
     function: () -> Unit = {},
-    isDrawerOpen: MutableState<Boolean> = mutableStateOf(true)
+    isDrawerOpen: MutableState<Boolean> = mutableStateOf(true),
+    scrollState: ScrollState = rememberScrollState()
 ) {
     fun onDismissRequest() {
         val it = isDrawerOpen.value
         isDrawerOpen.value = !it
     }
 
+    val conf = LocalConfiguration.current
+
     AnimatedVisibility(
         visible = isDrawerOpen.value,
-        enter = expandVertically(animationSpec = spring(dampingRatio = 2f)),
-        exit = shrinkVertically(animationSpec = spring(dampingRatio = 2f))
+        enter = expandVertically(spring(1.3f), Alignment.Bottom, initialHeight = { (it * 2.5).toInt() }),
+        exit = shrinkVertically(spring(1.3f), Alignment.Bottom, targetHeight = { (it * 3) })
     ) {
-        Surface(
-            shape = RoundedCornerShape(
-                topStart = 40.dp,
-                topEnd = 40.dp,
-                bottomStart = 0.dp,
-                bottomEnd = 0.dp
-            ),
-            color = Color.White,
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(1100.dp)
-                .padding(top = 250.dp)
+                .padding(top=150.dp)
+                .fillMaxWidth(1f)
+                .fillMaxHeight(1f)
+                .verticalScroll(scrollState)
+                .background(Color.White, RoundedCornerShape(
+                    topStart = 40.dp,
+                    topEnd = 40.dp,
+                    bottomStart = 0.dp,
+                    bottomEnd = 0.dp
+                ))
+            ,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(
-                modifier = Modifier.fillMaxWidth(1f)
-                    .padding(top=21.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+            Spacer(Modifier.height(21.dp))
+            TLine()
+            Spacer(Modifier.height(32.dp))
+            Text(
+                text = "TB_date",
+                style = TextStyle(
+                    fontSize = 10.sp,
+                    fontFamily = FontFamily(Font(R.font.inter)),
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFF727272),
+                    textAlign = TextAlign.Center
+                ),
+                modifier = Modifier
+                    .width(82.dp)
+                    .height(12.dp)
+            )
+            Spacer(modifier = Modifier.height(3.dp))
+
+            Text(
+                text = "TB_title",
+                style = TextStyle(
+                    fontSize = 25.sp,
+                    fontFamily = FontFamily(Font(R.font.inter)),
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFF454545)
+                )
+            )
+            // TB_location 텍스트
+            Row(
+                Modifier
+                    .align(Alignment.Start)
+                    .fillMaxWidth(1f)
+                    .padding(start = 108.dp, top = 7.dp)
             ) {
-                TLine()
-                Spacer(Modifier.height(32.dp))
+                CMLocation(
+                    modifier = Modifier
+                )
+                Spacer(Modifier.width(2.dp))
                 Text(
-                    text = "TB_date",
+                    text = "TB_location",
                     style = TextStyle(
-                        fontSize = 10.sp,
+                        fontSize = 13.sp,
+                        fontFamily = FontFamily(Font(R.font.inter)),
+                        fontWeight = FontWeight(600),
+                        color = Color(0xFF727272),
+                        textAlign = TextAlign.Center
+                    )
+                )
+            }
+            // TB_Who 텍스트
+            Row(
+                Modifier
+                    .align(Alignment.Start)
+                    .fillMaxWidth(1f)
+                    .padding(start = 108.dp, top = 13.dp)
+            ) {
+                CMWho(
+                    modifier = Modifier
+                )
+                Spacer(Modifier.width(3.dp))
+                Text(
+                    text = "TB_Who",
+                    style = TextStyle(
+                        fontSize = 13.sp,
                         fontFamily = FontFamily(Font(R.font.inter)),
                         fontWeight = FontWeight(600),
                         color = Color(0xFF727272),
                         textAlign = TextAlign.Center
                     ),
-                    modifier = Modifier
-                        .width(82.dp)
-                        .height(12.dp)
                 )
-                Spacer(modifier = Modifier.height(3.dp))
-
+            }
+            Box(
+                Modifier
+                    .width(325.dp)
+                    .defaultMinSize(minHeight = 100.dp)
+                    .align(Alignment.Start)
+                    .padding(top = 9.dp, start = 36.dp, end = 36.dp)
+            ) {
                 Text(
-                    text = "TB_title",
+                    text = "TB_text",
                     style = TextStyle(
-                        fontSize = 25.sp,
+                        fontSize = 10.sp,
                         fontFamily = FontFamily(Font(R.font.inter)),
-                        fontWeight = FontWeight(600),
-                        color = Color(0xFF454545)
+                        fontWeight = FontWeight(500),
+                        color = Color(0xFF000000),
+                        textAlign = TextAlign.Center,
                     )
                 )
-
-                Column(
-                    modifier = Modifier
-                        .padding(top=7.dp, start = 108.dp)
-                        .fillMaxWidth(1f)
-                        .height(150.dp)
-                ) {
-                    // TB_location 텍스트
-                    Row {
-                        CMLocation(
-                            modifier = Modifier
-                        )
-                        Spacer(Modifier.width(2.dp))
-                        Text(
-                            text = "TB_location",
-                            style = TextStyle(
-                                fontSize = 13.sp,
-                                fontFamily = FontFamily(Font(R.font.inter)),
-                                fontWeight = FontWeight(600),
-                                color = Color(0xFF727272),
-                                textAlign = TextAlign.Center
-                            )
-                        )
-                    }
-                    // TB_Who 텍스트
-                    Row(Modifier.padding(top=13.dp)) {
-                        CMWho(
-                            modifier = Modifier
-                        )
-                        Spacer(Modifier.width(3.dp))
-                        Text(
-                            text = "TB_Who",
-                            style = TextStyle(
-                                fontSize = 13.sp,
-                                fontFamily = FontFamily(Font(R.font.inter)),
-                                fontWeight = FontWeight(600),
-                                color = Color(0xFF727272),
-                                textAlign = TextAlign.Center
-                            ),
-                        )
-                    }
-                }
-
-                //예시 컴포넌트들
-                // eContext3()
             }
+            Spacer(Modifier.height(36.dp))
+            Box(
+                modifier = Modifier
+                    .width(140.dp)
+                    .height(140.dp)
+                    .background(color = Color(0xFFFFFFFF))
+            ) {
+                // 글자의 배경 박스를 노란색으로 설정
+                Box(
+                    modifier = Modifier
+                        .width(140.dp)
+                        .height(140.dp)
+                        .background(color = Color(0xFFD9D9D9))
+                        .align(Alignment.Center) // 중앙 정렬
+                        .padding(4.dp) // 텍스트와 박스와의 간격
+                ) {
+                    Text(
+                        text = "PB_image",
+                        style = TextStyle(
+                            fontSize = 15.sp,
+                            fontFamily = FontFamily(Font(R.font.inter)),
+                            fontWeight = FontWeight(500),
+                            color = Color(0xFF454545),
+                        ), modifier = Modifier
+                            .align(Alignment.Center) // 중앙 정렬
+                    )
+                }
+            }
+            Spacer(Modifier.height(36.dp))
         }
     }
 }
@@ -196,45 +257,3 @@ fun TLine(modifier: Modifier = Modifier) {
     )
 }
 
-
-// 실제 작동하는 컴포넌트가 아니라, 어느 위치 어느 크기로 작동될지 예시로 보여주는 컴포넌트들
-
-@Composable
-fun eContext3() {
-    Box(
-        modifier = Modifier
-            .width(200.dp)
-            .height(200.dp)
-    ) {
-        // 사각형 및 PB_image 텍스트
-        Box(
-            modifier = Modifier
-                .width(140.dp)
-                .height(140.dp)
-                .background(color = Color(0xFFFFFFFF))
-                .offset(y = (-30).dp)  // Y축으로 -50.dp만큼 올립니다.
-                .align(Alignment.BottomCenter) // 바닥 중앙에 배치
-        ) {
-            // 글자의 배경 박스를 노란색으로 설정
-            Box(
-                modifier = Modifier
-                    .width(140.dp)
-                    .height(140.dp)
-                    .background(color = Color(0xFFD9D9D9))
-                    .align(Alignment.Center) // 중앙 정렬
-                    .padding(4.dp) // 텍스트와 박스와의 간격
-            ) {
-                Text(
-                    text = "PB_image",
-                    style = TextStyle(
-                        fontSize = 15.sp,
-                        fontFamily = FontFamily(Font(R.font.inter)),
-                        fontWeight = FontWeight(500),
-                        color = Color(0xFF454545),
-                    ), modifier = Modifier
-                        .align(Alignment.Center) // 중앙 정렬
-                )
-            }
-        }
-    }
-}
