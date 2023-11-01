0812358a6d14ff956c1bf1d9ab07f2762559a660
soyeong <cjstk7441@naver.com>
2023-11-01 12:28:18 +0900

    20231101 MyInformationActivity 화면 추가

---
+++ app/src/main/java/kr/ac/kookmin/clouddrawing/MyInformationActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.kookmin.clouddrawing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_information)
+    }
+}
