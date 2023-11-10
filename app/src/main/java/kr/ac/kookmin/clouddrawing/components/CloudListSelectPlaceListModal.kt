8099843bcff3562571514cfbb010214889aea417
20203153 <123941587+20203153@users.noreply.github.com>
2023-11-10 16:13:34 +0900

    CloudListSeletPlaceList Modal add background

--- app/src/main/java/kr/ac/kookmin/clouddrawing/components/CloudListSelectPlaceListModal.kt
+++ app/src/main/java/kr/ac/kookmin/clouddrawing/components/CloudListSelectPlaceListModal.kt
@@ -6,21 +6,16 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -34,7 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
-
+import androidx.compose.ui.unit.dp
 
 
 @SuppressLint("UnrememberedMutableState")
@@ -82,7 +77,25 @@ fun CloudListSelectPlaceListModal(
         enter = expandVertically(spring(1.3f), Alignment.Bottom, initialHeight = { (it * 2.5).toInt() }),
         exit = shrinkVertically(spring(1.3f), Alignment.Bottom, targetHeight = { (it * 3) })
     ) {
-
+        Column(
+            modifier = Modifier
+                .padding(top = 150.dp)
+                .fillMaxWidth(1f)
+                .fillMaxHeight(1f)
+                .verticalScroll(scrollState)
+                .background(Color.White, RoundedCornerShape(
+                    topStart = 40.dp,
+                    topEnd = 40.dp,
+                    bottomStart = 0.dp,
+                    bottomEnd = 0.dp
+                )
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(Modifier.height(21.dp))
+            TLine()
+            Spacer(Modifier.height(32.dp))
+        }
     }
 }
 
